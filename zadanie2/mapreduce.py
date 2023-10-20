9dedf16dba75dc56bb93a5ee11bf8ff86f6a3ffc
BartoszP37 <103176742+BartoszP37@users.noreply.github.com>
2023-10-20 22:03:39 +0200

    zadanie2

---
+++ zadanie2/mapreduce.py
@@ -0,0 +1,35 @@
+from mrjob.job import MRJob
+
+class MovieRatingAverage(MRJob):
+    def mapper(self, _, line):
+        if not hasattr(self, 'movie_titles'):
+            self.movie_titles = {}
+            with open('C:/Users/Bartosz/Desktop/zadanie2/movies.csv', 'r', encoding='latin-1') as movies_file:
+                for movie_line in movies_file:
+                    movieId, title, genres = movie_line.strip().split(',', 2)
+                    self.movie_titles[movieId] = title
+
+        userId, movieId, rating, timestamp = line.split(",")
+        try:
+            rating = float(rating)
+            if movieId in self.movie_titles:
+                movie_title = self.movie_titles[movieId]
+                yield movie_title, (rating, 1)
+        except ValueError:
+            pass
+
+    def reducer(self, movie_title, rating_count_pairs):
+        total_rating, total_count = 0, 0
+        for rating, count in rating_count_pairs:
+            total_rating += rating
+            total_count += count
+        average_rating = total_rating / total_count if total_count > 0 else 0
+        yield movie_title, average_rating
+
+if __name__ == '__main__':
+    MovieRatingAverage.run()
+
+
+
+
+
